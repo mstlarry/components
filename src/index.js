80db78bd6057684ac53a2228727919daa8d5ae94
Larry <mst.larry091@gmail.com>
2020-05-23 02:47:42 +0100

    A React project to get familiar with components and props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <h4>Warning!!!</h4>
+        <div>Are you sure?</div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:45PM" 
+          content="Nice blog post"
+        />
+      </ApprovalCard>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          content="I like this subject"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          content="I like the writing"
+        />
+      </ApprovalCard>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
